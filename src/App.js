ae028ebc6afdc3efac1e5f81c587e51dbe6a08f8
Daniel Esteban Agudelo Cruz <de.agudelo@uniandes.edu.co>
2021-03-10 20:18:36 -0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import WebFont from "webfontloader";
+
+import { Container, Grid, Typography, createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import Content from "./Content";
+
+WebFont.load({
+  google: {
+    families: ["Poppins:100,300,400,500", "sans-serif"],
+  },
+});
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Poppins",
+  },
+  palette: {
+    primary: {
+      main: "#ff5000",
+    },
+    secondary: {
+      main: "#000",
+    },
+    warning: {
+      main: "#ffbf00",
+    },
+    error: {
+      main: "#eb4034",
+    },
+    success: {
+      main: "#76A30F",
+    },
+  },
+  // palette: {
+  //   primary: {
+  //     // Purple and green play nicely together.
+  //     main: purple[500],
+  //   },
+  //   secondary: {
+  //     // This is green.A700 as hex.
+  //     main: "#11cb5f",
+  //   },
+  // },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Grid container spacing={3} justify="center">
+          <Grid item container xs={12} justify="center">
+            <Typography
+              align="center"
+              color="secondary"
+              variant="h1"
+            >
+              Dogs
+            </Typography>
+          </Grid>
+          <Content/>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
